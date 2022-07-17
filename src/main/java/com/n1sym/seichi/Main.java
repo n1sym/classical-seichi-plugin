3682239c58cf90ee01ffae91bbccccb08dc61863
n1sym <owlbook248@gmail.com>
2022-07-17 17:12:27 +0900

    feat: inital settings

---
+++ src/main/java/com/n1sym/seichi/Main.java
@@ -0,0 +1,10 @@
+package com.n1sym.seichi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    getLogger().info("プラグインを有効にしました。");
+  }
+}
